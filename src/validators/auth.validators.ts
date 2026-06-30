dea52fb2d202973b6cfb930b35c9d04f15216174
Jibin <jibinjayachandran@gmail.com>
2026-06-30 17:07:21 +0400

    zod validation in auth flow (#2)
    * docker configs
    
    * zod validations

---
+++ src/validators/auth.validators.ts
@@ -0,0 +1,16 @@
+import {email, z} from 'zod';
+
+export const registerSchema = z.object({
+    email : z.email('Invalid email address'),
+    password:z.string().min(8,'Password must be at least 8 characters.'),
+    name: z.string().min(1,'name is required')
+});
+
+export const loginSchema = z.object({
+  email: z.email('Invalid email address'),
+  password: z.string().min(1, 'Password is required'),
+
+});
+
+export type RegisterInput = z.infer<typeof registerSchema>;
+export type LoginInput = z.infer<typeof loginSchema>;
