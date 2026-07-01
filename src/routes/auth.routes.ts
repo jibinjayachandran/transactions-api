a6e0007cb94597d1c2a6eb88e43c834fe8d86208
Jibin Jayachandran <jibinjayachandran@gmail.com>
2026-07-01 15:49:22 +0400

    refresh token and logout

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
-import { register, login } from '../controllers/auth.controller';
-import { registerSchema, loginSchema } from '../validators/auth.validators';
-import { validate } from '../middleware/auth.middleware';
+import { register, login, refresh, logout, logoutAll } from '../controllers/auth.controller';
+import { registerSchema, loginSchema, refreshSchema } from '../validators/auth.validators';
+import { protect, validate } from '../middleware/auth.middleware';
 
 const router = Router();
 
 router.post('/register', validate(registerSchema), register);
-
 router.post('/login', validate(loginSchema), login);
+router.post('/refreshToken', validate(refreshSchema), refresh);
+router.post('/logout', validate(refreshSchema), logout);
+router.post('/logout-all', protect, logoutAll);
 
 export default router;
